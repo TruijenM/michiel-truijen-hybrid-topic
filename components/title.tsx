a6fc49abda0cea0e547c02fe14978994aa5498b2
Michiel Truijen <michiel.truijen@student.thomasmore.be>
2025-01-27 16:44:49 +0100

    green-bg

--- components/title.tsx
+++ components/title.tsx
@@ -3,7 +3,7 @@ type TitleProps={
     title: string,
 }
 export function Title({title}:PropsWithChildren<TitleProps>) {
-    return<div className="text-center min-h0screen h-full flex flex0col justify-center gap-y-20 w-full bg-black text-white">
+    return<div className="text-center min-h0screen h-full flex flex0col justify-center gap-y-20 w-full bg-green-400 text-white">
         <h1 className="text-4xl font bold">{title}</h1>
     </div>
 }
